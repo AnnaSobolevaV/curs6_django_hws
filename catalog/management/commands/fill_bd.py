ed8763d0eb6ea27b5ccee4eb0207ee360b16db32
Anna Soboleva <asoboleva.avs@gmail.com>
2024-07-30 02:07:33 +0300

    hw 20.1 bugfix

--- catalog/management/commands/fill_bd.py
+++ catalog/management/commands/fill_bd.py
@@ -64,12 +64,16 @@ class Command(BaseCommand):
 
         # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
         for product in Command.json_read_products(data_json):
+            if product["fields"]["category"]:
+                category_product = Category.objects.get(pk=product["fields"]["category"])
+            else:
+                category_product = None
             product_for_create.append(
                 Product(id=product["pk"], name=product["fields"]["name"],
                         description=product["fields"]["description"],
                         image=product["fields"]["image"],
-                        category=Category.objects.get(pk=product["fields"]["category"]),
-                        price=product["fields"]["name"], created_at=product["fields"]["created_at"],
+                        category=category_product,
+                        price=product["fields"]["price"], created_at=product["fields"]["created_at"],
                         updated_at=product["fields"]["updated_at"])
             )
 
