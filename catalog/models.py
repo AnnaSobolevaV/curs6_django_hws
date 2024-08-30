246c8940eaba1b94eff8d6f3abc1cf7ce5a79d20
Anna Soboleva <asoboleva.avs@gmail.com>
2024-08-30 12:46:59 +0300

    hw 21.1 commit

--- catalog/models.py
+++ catalog/models.py
@@ -1,12 +1,102 @@
 from django.db import models
 
 
+class Contacts(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Ваше имя",
+        help_text=""
+    )
+    phone = models.CharField(
+        max_length=16,
+        verbose_name="Контактный телефон",
+        blank=True,
+        null=True
+    )
+    message = models.TextField(
+        verbose_name="Сообщение",
+        help_text="",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Обращение"
+        verbose_name_plural = "Обращения"
+
+    def __str__(self):
+        return self.name
+
+
+class BlogRecord(models.Model):
+    header = models.CharField(
+        max_length=50,
+        verbose_name="Заголовок",
+        help_text=""
+    )
+    slug = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+        verbose_name="slug",
+        help_text=""
+    )
+    content = models.TextField(
+        verbose_name="Контент",
+        help_text="",
+        blank=True,
+        null=True
+    )
+    preview = models.ImageField(
+        upload_to="blog_record/preview/",
+        blank=True,
+        null=True,
+        verbose_name="",
+        help_text="",
+    )
+    created_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name="Дата создания",
+        help_text=""
+    )
+    updated_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name="Дата последнего изменения",
+        help_text=""
+    )
+    is_published = models.BooleanField(
+        blank=True,
+        null=True,
+        verbose_name="Опубликовано",
+        help_text=""
+    )
+    count = models.IntegerField(
+        default=0,
+        verbose_name="Количество просмотров",
+        help_text=""
+    )
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+
+    def __str__(self):
+        return self.header
+
+
 class Category(models.Model):
     name = models.CharField(
-        max_length=50, verbose_name="Название категории", help_text="Название категории"
+        max_length=50,
+        verbose_name="Название категории",
+        help_text=""
     )
     description = models.TextField(
-        verbose_name="Описание категории", help_text="Описание категории", blank=True, null=True
+        verbose_name="Описание категории",
+        help_text="Описание категории",
+        blank=True,
+        null=True
     )
 
     class Meta:
@@ -19,11 +109,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(
-        max_length=50, verbose_name="Название продукта", help_text="Название продукта"
+        max_length=50,
+        verbose_name="Название продукта",
+        help_text=""
     )
     description = models.TextField(
         verbose_name="Описание продукта",
-        help_text="Описание продукта",
+        help_text="",
         blank=True,
         null=True,
     )
@@ -32,22 +124,27 @@ class Product(models.Model):
         blank=True,
         null=True,
         verbose_name="Фото продукта",
-        help_text="Фото продукта",
+        help_text="",
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         verbose_name="Категория",
-        help_text="Категория",
+        help_text="",
         blank=True,
         null=True,
         related_name="Products",
     )
     price = models.FloatField(
-        blank=True, null=True, verbose_name="Цена", help_text="Цена"
+        blank=True,
+        null=True,
+        verbose_name="Цена",
+        help_text=""
     )
     created_at = models.DateTimeField(
-        blank=True, null=True, verbose_name="Дата создания", help_text="Дата создания"
+        blank=True, null=True,
+        verbose_name="Дата создания",
+        help_text="Дата создания"
     )
     updated_at = models.DateTimeField(
         blank=True,
--- catalog/views.py
+++ catalog/views.py
@@ -1,35 +1,183 @@
-from django.shortcuts import render, get_object_or_404
-from catalog.models import Product, Category
+import datetime
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.utils.text import slugify
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from catalog.models import Category, Product, BlogRecord, Contacts
+from unidecode import unidecode
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'new message from {name}({email}): {message}')
+class ContactsCreateView(CreateView):
+    model = Contacts
+    template_name = 'catalog/contacts.html'
+    fields = ("name", "phone", "message")
+    success_url = reverse_lazy('catalog:contacts')
 
-    category_list = [{'name': 'подарки для вас'}, {'name': 'подарки для нас'}]
-    context = {
-        "category_list": category_list
-    }
-
-    return render(request, 'contacts.html', context)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
 
 
-def home(request):
-    product_list = Product.objects.all()
+def without_category(request):
+    product_list_wo_category = Product.objects.filter(category=None)
     category_list = Category.objects.all()
-    context = {
-        "product_list": product_list,
-        "category_list": category_list
-    }
-    return render(request, 'home.html', context)
+    context = {'product_list_wo_category': product_list_wo_category,
+               'category_list': category_list}
+    return render(request, 'catalog/category_detail.html', context)
 
 
-def product(request, pk):
-    product_ = get_object_or_404(Product, pk=pk)
-    context = {
-        "product_": product_
-    }
-    return render(request, 'product.html', context)
+class CategoryDetailView(DetailView):
+    model = Category
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        product_list = Product.objects.filter(category=self.object.id)
+        context['product_list'] = product_list
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+
+class ProductListView(ListView):
+    model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ("name", "category", "price", "image", "description")
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ("name", "category", "price", "image", "description")
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+
+class BlogRecordListView(ListView):
+    model = BlogRecord
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+class BlogRecordDetailView(DetailView):
+    model = BlogRecord
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogRecordCreateView(CreateView):
+    model = BlogRecord
+    fields = ("header", "is_published", "content", "preview")
+    success_url = reverse_lazy('catalog:blogrecord_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_rec = form.save()
+            new_rec.slug = slugify(unidecode(new_rec.header + ' ' + str(new_rec.pk)))
+            new_rec.created_at = datetime.date.today()
+            new_rec.save()
+
+        return super().form_valid(form)
+
+
+class BlogRecordUpdateView(UpdateView):
+    model = BlogRecord
+    fields = ("header", "is_published", "content", "preview")
+    success_url = reverse_lazy('catalog:blogrecord_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_rec = form.save()
+            new_rec.slug = slugify(unidecode(new_rec.header + ' ' + str(new_rec.pk)))
+            new_rec.updated_at = datetime.datetime.now()
+            new_rec.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('catalog:blogrecord', args=[self.kwargs.get('pk')])
+
+
+class BlogRecordDeleteView(DeleteView):
+    model = BlogRecord
+    success_url = reverse_lazy('catalog:blogrecord_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        category_list = Category.objects.all()
+        context['category_list'] = category_list
+        return context
